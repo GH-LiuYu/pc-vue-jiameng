52345c5f864394c4eb5e2d4763afca59a1c4b325
Your Name <email@example.com>
2022-01-04 00:13:57 +0800

    '20210103'

--- src/view/Home/Index.vue
+++ src/view/Home/Index.vue
@@ -1,17 +1,48 @@
 <template>
-    <div class="pageTop">
-        <div class="wrap_mar clear">
-            <ul class="fl pageTop-left">
-                <li><a href="http://jmw.com.cn/" title="加盟网首页" target="_blank" style="padding-left:0;"><i class="zongIcon i6 fl"></i>首页</a>|</li>
-                <li><a href="http://cae.jmw.com.cn/" target="_blank" title="加盟产业博览会|嘉年华">加盟产业博览会|嘉年华</a>|</li>
-                <li><a href="http://top.jmw.com.cn/" target="_blank" title="排行榜">排行榜</a>|</li>
-                <li><a href="http://www.jmw.com.cn/area/" target="_blank" title="城市站">城市站</a>|</li>
-                <li><a href="http://vc.jmw.com.cn/" target="_blank" title="创业">创业</a>|</li>
-                <li><a href="http://dl.jmw.com.cn/" target="_blank" title="代理">代理</a>|</li>
-                <li><a href="http://ls.jmw.com.cn/" target="_blank" title="连锁">连锁</a>|</li>
-            </ul>
+    <div>
+        <div class="pageTop">
+            <div class="wrap_mar clear">
+                <ul class="fl pageTop-left">
+                    <li><a href="http://jmw.com.cn/" title="加盟网首页" target="_blank" style="padding-left:0;"><i class="zongIcon i6 fl"></i>首页</a>|</li>
+                    <li><a href="http://cae.jmw.com.cn/" target="_blank" title="加盟产业博览会|嘉年华">加盟产业博览会|嘉年华</a>|</li>
+                    <li><a href="http://top.jmw.com.cn/" target="_blank" title="排行榜">排行榜</a>|</li>
+                    <li><a href="http://www.jmw.com.cn/area/" target="_blank" title="城市站">城市站</a>|</li>
+                    <li><a href="http://vc.jmw.com.cn/" target="_blank" title="创业">创业</a>|</li>
+                    <li><a href="http://dl.jmw.com.cn/" target="_blank" title="代理">代理</a>|</li>
+                    <li><a href="http://ls.jmw.com.cn/" target="_blank" title="连锁">连锁</a>|</li>
+                </ul>
+            </div>
+        </div>
+        <div class="wrap_mar mainStart" style="margin-top: 45px;">
+            <div class="logoSearch clear">
+                <a href="http://www.jmw.com.cn/" title="加盟网" class="logo fl" target="_blank"><img class="lazy" src="http://image1.jmw.com.cn/newzonghe/images/logo_27.png" width="410" height="49" data-url="http://image1.jmw.com.cn/newzonghe/images/logo_27.png"></a>
+
+                <p class="fr true_brand"><a href="http://www.jmw.com.cn/toutiao/17668168.html" title="网站名称变更" target="_blank"><img class="lazy" src="http://attachment4.jmw.com.cn/image/2021/11/24/16377440191867.gif" width="210" height="50" data-url="http://attachment4.jmw.com.cn/image/2021/11/24/16377440191867.gif" alt="1831资质认证">
+
+
+                </a>
+                </p>
+                <div class="logoFind fr">
+                    <span class="hangYe fl">
+                <em class="fl">行业</em><i class="fl hyIcon i64_hy"></i>
+            </span>
+                    <ul class="indusXiala clear">
+                        <li><a href="http://www.jmw.com.cn/category/canyin/" title="餐饮加盟" target="_blank">餐饮</a></li><li><a href="http://www.jmw.com.cn/category/jiaoyu/" title="教育加盟" target="_blank">教育</a></li><li><a href="http://www.jmw.com.cn/category/youer/" title="幼儿加盟" target="_blank">幼儿</a></li><li><a href="http://www.jmw.com.cn/category/meirong/" title="美容加盟" target="_blank">美容</a></li><li><a href="http://www.jmw.com.cn/category/fuzhuang/" title="服装加盟" target="_blank">服装</a></li><li><a href="http://www.jmw.com.cn/category/jiudian/" title="酒店加盟" target="_blank">酒店</a></li><li><a href="http://www.jmw.com.cn/category/ganxi/" title="干洗加盟" target="_blank">干洗</a></li><li><a href="http://www.jmw.com.cn/category/zhubaoshipin/" title="珠宝饰品加盟" target="_blank">珠宝饰品</a></li><li><a href="http://www.jmw.com.cn/category/jiaju/" title="家居加盟" target="_blank">家居</a></li><li><a href="http://www.jmw.com.cn/category/jiafang/" title="家纺加盟" target="_blank">家纺</a></li><li><a href="http://www.jmw.com.cn/category/huanbao/" title="环保加盟" target="_blank">环保</a></li><li><a href="http://www.jmw.com.cn/category/jiancai/" title="建材加盟" target="_blank">建材</a></li><li><a href="http://www.jmw.com.cn/category/qiche/" title="汽车加盟" target="_blank">汽车</a></li><li><a href="http://www.jmw.com.cn/category/shipin1/" title="食品加盟" target="_blank">食品</a></li><li><a href="http://www.jmw.com.cn/category/lingshou/" title="零售加盟" target="_blank">零售</a></li><li><a href="http://www.jmw.com.cn/category/chaoshi/" title="超市加盟" target="_blank">超市</a></li><li><a href="http://www.jmw.com.cn/category/jiushui/" title="酒水加盟" target="_blank">酒水</a></li><li><a href="http://www.jmw.com.cn/category/xinhangye/" title="新行业加盟" target="_blank">新行业</a></li><li><a href="http://www.jmw.com.cn/category/jingpin/" title="精品加盟" target="_blank">精品</a></li><li><a href="http://www.jmw.com.cn/category/shilibaojian/" title="视力保健加盟" target="_blank">视力保健</a></li>                    </ul>
+                    <span class="fl line"></span>
+                    <input type="text" autocomplete="off" maxlength="20" value="行业/品牌/关键词" searchautocomplete="true" onblur="if(this.value==''){this.value='行业/品牌/关键词';}" onfocus="if(this.value=='行业/品牌/关键词'){this.value='';}" name="keyword" id="keyword_fenci" class="text fl ui-autocomplete-input">
+                    <ul class="bottomLa"> <li><span>热门搜索</span></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6623384/">雪墨干洗&nbsp;<img class="bottomLaimg" src="http://image1.jmw.com.cn/1831icon/threeStar.png"></a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6773095/">自然美美容院&nbsp;<img class="bottomLaimg" src="http://image1.jmw.com.cn/1831icon/threeStar.png"></a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6671/">快乐星汉堡&nbsp;<img class="bottomLaimg" src="http://image1.jmw.com.cn/1831icon/threeStar.png"></a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6869065/">中康国际体检中心&nbsp;<img class="bottomLaimg" src="http://image1.jmw.com.cn/1831icon/threeStar.png"></a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6667894/">三草两木&nbsp;<img class="bottomLaimg" src="http://image1.jmw.com.cn/1831icon/threeStar.png"></a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6873102/">iEnglish智慧学习终端&nbsp;<img class="bottomLaimg" src="http://image1.jmw.com.cn/1831icon/threeStar.png"></a></li>
+                    </ul>
+                    <input type="button" value="搜索" onclick="goSub($(this),2)" class="btn fl">
+                    <ul class="nav_bot"><li><a target="_blank" href="http://www.jmw.com.cn/xm6872260/">特荬特折扣便利店</a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6828784/">撞色奶茶</a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6872125/">莱顿洗衣</a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6871505/">萌小它宠物店</a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6795538/">书颜世家皮肤管理</a></li><li><a target="_blank" href="http://www.jmw.com.cn/xm6815900/">飞越眼镜</a></li>
+
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
+
+
+
 </template>
 
 <script>
@@ -25,7 +56,26 @@
 <style src="../../assets/css/reset.css">
 
 </style>
-<style scoped>
+<style src="../../assets/css/change.css">
 
+</style>
+<style scoped>
+    .nav_bot {
+        width: 100%;
+        overflow: hidden;
+        height: 30px;
+        line-height: 30px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .true_brand {
+         width: 210px;
+         height: 50px;
+         margin-top: 20px;
+         border-radius: 1px;
+         overflow: hidden;
+     }
 </style>
 
